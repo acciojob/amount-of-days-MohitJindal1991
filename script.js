b4671895faeae8207085f88774baeac8c1ee2581
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-05 17:04:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(num){
+	if(num%400==0){
+		return 366;
+	}
+	else if(num%4==0 && num%100!=0){
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
